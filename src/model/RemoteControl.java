321dd27bbebdb7d2197b626718cddffa9cc737ce
roberts <gabrielrobertssoares@gmail.com>
2024-07-31 23:08:09 -0300

    classe abstrata de controle remoto

--- src/model/RemoteControl.java
+++ src/model/RemoteControl.java
@@ -1,7 +1,24 @@
 package model;
 
-public interface RemoteControl {
-    void on();
-    void off();
-    boolean checkEletronicOn();
+public abstract class RemoteControl {
+
+    protected Device device;
+
+    public RemoteControl(Device device) {
+        this.device = device;
+    }
+
+    public void isOn(){
+        device.on();
+        System.out.println(device.getClass().getSimpleName() + " is now on");
+    }
+
+    public void isOff(){
+        device.off();
+        System.out.println(device.getClass().getSimpleName() + " is now off");
+    }
+
+    public boolean checkDeviceOn(){
+        return device.checkIsOn();
+    }
 }
